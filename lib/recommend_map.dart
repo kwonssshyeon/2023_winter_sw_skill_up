cb9736bcd6f889da776da7a1992d403cf4f968b6
kwonssshyeon <kwonsh4396@gmail.com>
2023-02-03 07:21:45 +0900

    map2

---
+++ lib/recommend_map.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:winter_skillup_hackathon/mywalk_list.dart';
+
+class RecommendMap extends StatefulWidget {
+  @override
+  State<RecommendMap> createState() => RecommendMapState();
+}
+
+class RecommendMapState extends State<RecommendMap> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  // 초기 카메라 위치
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex, // 초기 카메라 위치
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        polylines: ,
+      ),
+
+
+
+      // floatingActionButton을 누르게 되면 _goToTheLake 실행된다.
+      floatingActionButton: Row(
+        children: [
+
+        ],
+      ),
+
+
+
+    );
+  }
+
+}
